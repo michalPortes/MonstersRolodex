2c17f9c8ce0efc670551cd75c1be80a56eb4ad91
Michael Portes <michael11portes@hotmail.com>
2022-07-05 19:59:29 -0300

    Inicio de um rolodex de cards em react, aplicando o conceito de component e variacoes do react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CardList from './components/card-list/card-list.components'
+import SearchBox from './components/search-box/search-box.components'
+import './App.css'
+
+//Código para estudo, comentado e aplicado
+
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      monsters: [], // monsters é a lista que está vindo da API que se encontra em 'componentDidMount'
+      searchField: '' // paramentro que recebe a lista atualizada dos monstros, pra atualização ser realmente um filtro e não uma perda total da lista atual de monstros
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users') // URL onde se encontra o código que possui a API
+      .then(response => response.json())
+      .then(users =>
+        this.setState(() => {
+          return { monsters: users } // desde o primeiro parametro then, ele resgata o .json depois o user e assim atribui o user na lista monsters
+        })
+      )
+  }
+
+  onSearchChange = event => {
+    const searchField = event.target.value.toLocaleLowerCase() //toLocaleLowerCase "impede" a mudaça de letras minusculas para maiusculas
+    this.setState(() => {
+      return { searchField }
+    })
+  }
+
+  render() {
+    const { monsters, searchField } = this.state
+    const { onSearchChange } = this
+
+    const filteredMonsters = monsters.filter(monster => {
+      return monster.name.toLocaleLowerCase().includes(searchField) //filtragem dos monstros no seach
+    })
+
+    return (
+      <div className="App">
+        <h1 className="app-title">Monsters Rolidex</h1>
+        <SearchBox
+          className="monsters-search-box"
+          onChangeHandler={onSearchChange}
+          placeholder="Search monster"
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
